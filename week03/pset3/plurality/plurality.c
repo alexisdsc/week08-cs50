e328dbe50291879529fc93f50cb6de7959cea728
Aaron <iaaron.xyz@outlook.com>
2021-10-10 01:50:27 -0500

    Lab 3 finished and week 3 in progress.

---
+++ week03/pset3/plurality/plurality.c
@@ -0,0 +1,129 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+// Max number of candidates
+#define MAX 9
+
+// Candidates have name and vote count
+typedef struct
+{
+    string name;
+    int votes;
+}
+candidate;
+
+// Array of candidates
+candidate candidates[MAX];
+
+// Number of candidates
+int candidate_count;
+
+// Function prototypes
+bool vote(string name);
+void print_winner(void);
+int max_number_votes(void);
+
+int main(int argc, string argv[])
+{
+    // Check for invalid usage
+    if (argc < 2)
+    {
+        printf("Usage: plurality [candidate ...]\n");
+        return 1;
+    }
+
+    // Populate array of candidates
+    candidate_count = argc - 1;
+    if (candidate_count > MAX)
+    {
+        printf("Maximum number of candidates is %i\n", MAX);
+        return 2;
+    }
+    for (int i = 0; i < candidate_count; i++)
+    {
+        candidates[i].name = argv[i + 1];
+        candidates[i].votes = 0;
+    }
+
+    int voter_count = get_int("Number of voters: ");
+
+    // Loop over all voters
+    for (int i = 0; i < voter_count; i++)
+    {
+        string name = get_string("Vote: ");
+
+        // Check for invalid vote
+        if (!vote(name))
+        {
+            printf("Invalid vote.\n");
+        }
+    }
+
+    // Display winner of election
+    print_winner();
+}
+
+// Update vote totals given a new vote
+bool vote(string name)
+{
+    // TODO
+    int name_len = strlen(name);
+    int candidate_len;
+    // Quantity of candidates
+    for (int i = 0; i < candidate_count; i++)
+    {
+        // Check strings have the same length
+        candidate_len = strlen(candidates[i].name);
+        if (name_len != candidate_len)
+        {
+            continue;
+        }
+
+        // Check every letter match to each other
+        for (int j = 0; j < name_len; j++)
+        {
+            if (tolower(name[j]) != tolower(candidates[i].name[j]))
+            {
+                break;
+            }
+            if (j == name_len - 1)
+            {
+                candidates[i].votes += 1;
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+// Print the winner (or winners) of the election
+void print_winner(void)
+{
+    // TODO
+    int max_votes = max_number_votes();
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (candidates[i].votes == max_votes)
+        {
+            printf("%s\n", candidates[i].name);
+        }
+    }
+    return;
+}
+
+
+// Get the max number of votes of all candidates
+int max_number_votes(void)
+{
+    int max_votes = 0;
+    for (int i = 0; i < candidate_count; i++)
+        if (candidates[i].votes > max_votes)
+        {
+            max_votes = candidates[i].votes;
+        }
+
+    return max_votes;
+}
+
